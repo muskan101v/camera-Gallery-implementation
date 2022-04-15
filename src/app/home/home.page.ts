4be2918fc141fba52d81fb87cd720039f6ccd386
Muskan Varshney <muskanvarshney@GS-5876.local>
2022-04-15 18:25:39 +0530

    implement camera functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+/* eslint-disable no-debugger */
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild('filePicker', { static: false })
+  filePickerRef: ElementRef<HTMLInputElement>;
+  selectedImage;
+  usePicker = false;
+  constructor(private platform: Platform) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    console.log('Mobile:', this.platform.is('mobile'));
+    console.log('Hybrid:', this.platform.is('hybrid'));
+    console.log('iOS:', this.platform.is('ios'));
+    console.log('Android:', this.platform.is('android'));
+    console.log('Desktop:', this.platform.is('desktop'));
 
+    if (this.platform.is('desktop')) {
+      this.usePicker = true;
+    }
+  }
+  onPickImage() {
+    if (!Capacitor.isPluginAvailable('Camera')) {
+      return;
+    }
+    Camera.getPhoto({
+      quality: 50,
+      source: CameraSource.Prompt,
+      correctOrientation: true,
+      width: 300,
+      resultType: CameraResultType.DataUrl,
+    })
+      .then((image) => {
+        this.selectedImage = image.dataUrl;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  onFileChosen(event: Event) {
+    console.log(event);
+  }
 }
