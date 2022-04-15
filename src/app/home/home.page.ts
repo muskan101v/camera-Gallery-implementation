f699b43ea3a35de9ebd5f42e6dbfee9433c1e269
Muskan Varshney <muskanvarshney@GS-5876.local>
2022-04-15 21:51:42 +0530

    add PWA Element

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,30 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
 /* eslint-disable no-debugger */
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Capacitor } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
 
+function base64toBlob(base64Data, contentType) {
+  contentType = contentType || '';
+  const sliceSize = 1024;
+  const byteCharacters = window.atob(base64Data);
+  const bytesLength = byteCharacters.length;
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
+  const byteArrays = new Array(slicesCount);
+
+  for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+    const begin = sliceIndex * sliceSize;
+    const end = Math.min(begin + sliceSize, bytesLength);
+
+    const bytes = new Array(end - begin);
+    for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+      bytes[i] = byteCharacters[offset].charCodeAt(0);
+    }
+    byteArrays[sliceIndex] = new Uint8Array(bytes);
+  }
+  return new Blob(byteArrays, { type: contentType });
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -23,7 +44,10 @@ export class HomePage implements OnInit {
     console.log('Android:', this.platform.is('android'));
     console.log('Desktop:', this.platform.is('desktop'));
 
-    if (this.platform.is('desktop')) {
+    if (
+      (this.platform.is('mobile') && !this.platform.is('hybrid')) ||
+      this.platform.is('desktop')
+    ) {
       this.usePicker = true;
     }
   }
@@ -47,6 +71,17 @@ export class HomePage implements OnInit {
   }
 
   onFileChosen(event: Event) {
-    console.log(event);
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if (!pickedFile) {
+      return;
+    }
+    const fr = new FileReader();
+
+    fr.onload = () => {
+      const dataUrl = fr.result.toString();
+      this.selectedImage = dataUrl;
+    };
+
+    fr.readAsDataURL(pickedFile);
   }
 }
